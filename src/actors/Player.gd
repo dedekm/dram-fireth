23b8a697448c5b106e5908f80923a22c4ea247db
dedekm <dedekmm@gmail.com>
2020-04-20 19:04:58 +0200

    add y sort node to level,, tweak object origins

--- src/actors/Player.gd
+++ src/actors/Player.gd
@@ -21,10 +21,10 @@ func get_input() -> void:
 	velocity = Vector2()
 	if Input.is_action_pressed('right'):
 		velocity.x += 1
-		pick_up_area.position.x = 10
+		pick_up_area.position.x = 8
 	if Input.is_action_pressed('left'):
 		velocity.x -= 1
-		pick_up_area.position.x = -10
+		pick_up_area.position.x = -8
 	if Input.is_action_pressed('down'):
 		velocity.y += 1
 	if Input.is_action_pressed('up'):
