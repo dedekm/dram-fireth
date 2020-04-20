3d04cc9ce65b5eb6885a623fc3997e909cf27fbb
dedekm <dedekmm@gmail.com>
2020-04-21 01:27:17 +0200

    add asmr :]

--- src/objects/Fire.gd
+++ src/objects/Fire.gd
@@ -2,6 +2,17 @@ extends StaticBody2D
 
 export (int) var power = 1
 
+var asmr_timer: Timer
+var asmr_files: = [
+	preload("res://assets/sounds/asmr/ajajajajaaj.wav"),
+	preload("res://assets/sounds/asmr/burnwithme.wav"),
+	preload("res://assets/sounds/asmr/buuuuurn.wav"),
+	preload("res://assets/sounds/asmr/iwantmorewood.wav")
+]
+
+func _ready():
+	$FireSoundPlayer.play()
+
 func _process(_delta: float) -> void:
 	if power <= 0:
 		# TODO
@@ -16,12 +27,27 @@ func burn(object: Node) -> void:
 	if object.flammable:
 		power += object.mass
 		object.burn()
+		
+		asmr_timer = Timer.new()
+		asmr_timer.connect('timeout', self, '_play_asmr_sound')
+		add_child(asmr_timer)
+		asmr_timer.start(1)
 	else:
 		power -= object.mass
 		
 	print_debug('Fire power' + String(power))
 	
 	_update_particles()
+
+func _play_asmr_sound() -> void:
+	var i: = randi() % asmr_files.size()
+	var res = asmr_files[i]
+#	asmr_files.remove(i)
+	$AsmrPlayer.stream = res
+	$AsmrPlayer.play()
+	
+	asmr_timer.queue_free()
+	
 
 func _on_FireArea_body_entered(body: Node) -> void:
 	burn(body)
